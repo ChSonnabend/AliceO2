a075de89078ba6f04a1ce4b2e11df5ed00726934
Christian Sonnabend <sonnabendch@gmail.com>
2023-02-07 21:27:43 +0100

    Adding readMCtoTree task

---
+++ Detectors/TPC/workflow/src/readMCtoTree.cxx
@@ -0,0 +1,69 @@
+#include "DataFormatsTPC/WorkflowHelper.h"
+#include "TPCWorkflow/ProcessingHelpers.h"
+#include "SimulationDataFormat/MCCompLabel.h"
+#include "SimulationDataFormat/ConstMCTruthContainer.h"
+#include "SimulationDataFormat/LabelContainer.h"
+#include "SimulationDataFormat/IOMCTruthContainerView.h"
+#include "Framework/Logger.h"
+#include "Framework/Task.h"
+#include "Framework/DataProcessorSpec.h"
+#include "Framework/ConfigParamRegistry.h"
+
+using namespace o2;
+using namespace o2::framework;
+
+class readMCtruth : public Task
+{
+    public:
+        void init(InitContext& ic) final;
+        void run(ProcessingContext& pc) final;
+    private:
+        std::string infile = "tpcdigits.root";
+        std::string outfile = "mclabels.root";
+};
+
+void readMCtruth::init(InitContext& ic){
+    infile = ic.options().get<std::string>("input-file");
+    outfile = ic.options().get<std::string>("output-file");
+}
+
+void readMCtruth::run(ProcessingContext& pc){
+
+    // Reading...
+    TFile fin(infile.c_str(), "OPEN");
+    auto treein = (TTree*)fin.Get("o2sim");
+    dataformats::IOMCTruthContainerView* io = nullptr;
+    auto cont = o2::dataformats::MCLabelIOHelper::loadFromTTree(treein, "Labels", 0);
+
+    // Writing...
+    TFile fout(outfile.c_str(), "RECREATE");
+    TTree treeout("o2sim", "o2sim");
+    auto br = treeout.Branch("Labels", &io, 32000, 2);
+    treeout.Fill();
+    treeout.Write();
+    fout.Close();
+
+}
+
+#include "Framework/runDataProcessing.h"
+
+DataProcessorSpec readMonteCarloLabels()
+{
+  std::vector<InputSpec> inputs;
+  std::vector<OutputSpec> outputs;
+
+  return DataProcessorSpec{
+    "tpc-mc-labels",
+    inputs,
+    outputs,
+    adaptFromTask<readMCtruth>(),
+    Options{
+      {"input-file", VariantType::String, "tpcdigits.root", {"Input file name"}},
+      {"output-file", VariantType::String, "mclabels.root", {"Input file name"}}
+    }};
+}
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+    return WorkflowSpec{readMonteCarloLabels()};
+}
