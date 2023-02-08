93539eced4afce370158fc66aaa67b0b8437a1a1
Christian Sonnabend <sonnabendch@gmail.com>
2023-02-08 15:21:37 +0100

    changing run() to process()

--- Detectors/TPC/workflow/src/readMCtoTree.cxx
+++ Detectors/TPC/workflow/src/readMCtoTree.cxx
@@ -27,22 +27,27 @@ void readMCtruth::init(InitContext& ic){
     outfile = ic.options().get<std::string>("output-file");
 }
 
-void readMCtruth::run(ProcessingContext& pc){
+void readMCtruth::process(ProcessingContext& pc){
 
     // Reading...
     TFile fin(infile.c_str(), "OPEN");
+    LOG(info) << "Opened infile...";
     auto treein = (TTree*)fin.Get("o2sim");
+    LOG(info) << "Read the tree...";
     dataformats::IOMCTruthContainerView* io = nullptr;
     auto cont = o2::dataformats::MCLabelIOHelper::loadFromTTree(treein, "Labels", 0);
+    LOG(info) << "Container initialized and Labels loaded...";
 
     // Writing...
     TFile fout(outfile.c_str(), "RECREATE");
+    LOG(info) << "Writing to outfile...";
     TTree treeout("o2sim", "o2sim");
     auto br = treeout.Branch("Labels", &io, 32000, 2);
+    LOG(info) << "Writing labels";
     treeout.Fill();
     treeout.Write();
     fout.Close();
-
+    LOG(info) << "Done!";
 }
 
 #include "Framework/runDataProcessing.h"
