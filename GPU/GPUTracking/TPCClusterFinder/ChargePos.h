6cdd65e4b707fb6ffa6f8be86b5d9dd86ae73f12
Christian Sonnabend <sonnabendch@gmail.com>
2025-01-08 21:38:11 +0100

    Bug-fix

--- GPU/GPUTracking/TPCClusterFinder/ChargePos.h
+++ GPU/GPUTracking/TPCClusterFinder/ChargePos.h
@@ -49,8 +49,6 @@ struct ChargePos {
   GPUdi() tpccf::TPCFragmentTime time() const { return timePadded - GPUCF_PADDING_TIME; }
   GPUdi() tpccf::TPCFragmentTime globalTime() const { return timePadded; }
 
-  GPUdi() tpccf::TPCFragmentTime globalTime() const { return timePadded; }
-
  private:
   // Maps the position of a pad given as row and index in that row to a unique
   // index between 0 and TPC_NUM_OF_PADS.
