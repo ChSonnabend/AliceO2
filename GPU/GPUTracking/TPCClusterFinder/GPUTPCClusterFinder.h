89bd3af54acf168e337da6bddab2dd74febad68e
Christian Sonnabend <sonnabendch@gmail.com>
2025-03-15 10:57:26 +0100

    Adjusting elements to match dev

--- GPU/GPUTracking/TPCClusterFinder/GPUTPCClusterFinder.h
+++ GPU/GPUTracking/TPCClusterFinder/GPUTPCClusterFinder.h
@@ -19,10 +19,6 @@
 #include "GPUProcessor.h"
 #include "GPUDataTypes.h"
 #include "CfFragment.h"
-#include "ML/OrtInterface.h"
-#include "ML/3rdparty/GPUORTFloat16.h"
-
-using namespace o2::ml;
 
 namespace o2
 {
@@ -142,33 +138,6 @@ class GPUTPCClusterFinder : public GPUProcessor
   int16_t mZSId = -1;
   int16_t mZSOffsetId = -1;
   int16_t mOutputId = -1;
-
-  int nnClusterizerSizeInputRow = 3;
-  int nnClusterizerSizeInputPad = 3;
-  int nnClusterizerSizeInputTime = 3;
-  int nnClusterizerElementSize = -1;
-  bool nnClusterizerAddIndexData = true;
-  float nnClassThreshold = 0.16;
-  bool nnSigmoidTrafoClassThreshold = 1;
-  int nnClusterizerUseCFregression = 0;
-  int nnClusterizerBatchedMode = 1;
-  int nnClusterizerVerbosity = 0;
-  int nnClusterizerBoundaryFillValue = -1;
-  int nnClusterizerDumpDigits = 0;
-  int nnClusterizerApplyCfDeconvolution = 0;
-
-  // Memory allocation for neural network
-  uint class2_elements = 0;
-  std::vector<float> inputData32;
-  std::vector<OrtDataType::Float16_t> inputData16;
-  std::vector<float> outputDataClass, modelProbabilities, outputDataReg1, outputDataReg2;
-
-  std::vector<ChargePos> peakPositions;
-  std::vector<std::vector<int>> clusterFlags; // mSplitInTime, mSplitInPad. Techincally both flags are set in the same way -> ClusterAccumulator.cxx
-  std::vector<float> centralCharges;
-
-  std::unordered_map<std::string, std::string> OrtOptions;
-  OrtModel model_class, model_reg_1, model_reg_2; // For splitting clusters
 
 #ifndef GPUCA_GPUCODE
   void DumpDigits(std::ostream& out);
