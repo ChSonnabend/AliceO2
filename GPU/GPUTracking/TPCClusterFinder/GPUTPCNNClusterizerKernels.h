50bb8af9ca57c820cb2b990cc4904ddf9cb909a8
Christian Sonnabend <sonnabendch@gmail.com>
2025-03-19 16:04:07 +0100

    Bug-fix for isBoundary

--- GPU/GPUTracking/TPCClusterFinder/GPUTPCNNClusterizerKernels.h
+++ GPU/GPUTracking/TPCClusterFinder/GPUTPCNNClusterizerKernels.h
@@ -72,11 +72,10 @@ class GPUTPCNNClusterizerKernels : public GPUKernelTemplate
 
   static GPUd() int padOffset(int, int);
   static GPUd() int rowOffset(int, int);
-  static GPUd() bool isBoundary(int, int, int, const GPUTPCGeometry&);
+  static GPUd() bool isBoundary(int, int, int, int);
 
   static GPUdi() uint8_t countPeaksInner(uint16_t, const uint8_t*, uint8_t*);
   static GPUdi() uint8_t countPeaksOuter(uint16_t, uint8_t, const uint8_t*);
-  static GPUd() bool isBoundary(int, int, int);
 };
 
 } // namespace o2::gpu
