d057b681f7fc92bbc067a798da09e0beb114ad23
David Rohr <drohr@jwdt.org>
2025-09-29 22:01:44 +0200

    GPU TPC: Implement ambiguity solving for rebuild tracks cluster association

--- GPU/GPUTracking/qa/GPUQA.cxx
+++ GPU/GPUTracking/qa/GPUQA.cxx
@@ -1805,7 +1805,7 @@ void GPUQA::RunQA(bool matchOnly, const std::vector<o2::tpc::TrackTPC>* tracksEx
     }
   }
 
-  uint32_t nCl = clNative ? clNative->nClustersTotal : mTracking->GetProcessors()->tpcMerger.NMaxClusters();
+  uint32_t nCl = clNative ? clNative->nClustersTotal : mTracking->GetProcessors()->tpcMerger.NClusters();
   mClusterCounts.nTotal += nCl;
   if (mQATasks & taskClusterCounts) {
     for (uint32_t iSector = 0; iSector < GPUCA_NSECTORS; iSector++) {
